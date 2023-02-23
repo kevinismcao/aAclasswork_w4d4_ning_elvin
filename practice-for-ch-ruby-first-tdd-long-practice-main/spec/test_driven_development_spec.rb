b97ffa98d70e7528951c59e3c0c1cd8884928b43
ningxiao <dsgrichard@hotmail.com>
2023-02-23 14:19:05 -0800

    Myuniq twosum done

---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/test_driven_development_spec.rb
@@ -0,0 +1,34 @@
+require "test_driven_development"
+
+RSpec.describe 'test_driven_development' do 
+    describe '#my_uniq' do
+        it 'should return an array' do
+            expect(my_uniq([1, 2, 1, 3, 3])).to be_an(Array)
+        end
+
+        it 'should return an array with no duplicate' do
+            expect(my_uniq([1, 2, 1, 3, 3])).to eq([1, 2, 3])
+        end
+    end
+
+    describe 'Array#two_sum' do
+        it 'should return an array' do
+            expect([-1, 0, 2, -2, 1]).two_sum).to be_an(Array)
+        end
+        
+        it 'should return an array with no duplicate' do 
+            expect([-1, 0, 2, -2, 1]).two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+
+end
